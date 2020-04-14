aeaefc9a7a975f516b35f8d5fb1a431040f3be65
sovpro <its.so@outlook.com>
2020-04-14 12:31:18 -0400

    Fix export

--- src/zclopts.ts
+++ src/zclopts.ts
@@ -6,11 +6,13 @@ import {
 } from './types'
 import { reduceArgs, reduceOptions } from './reducers'
 
+export = zclopts
+
 /**
  * Produces an object with properties representing
  * program arguments and parameter values.
  */
-export default function zClopts (params: string[] | unknown): ProgramOptionsConfig {
+function zclopts (params: string[] | unknown): ProgramOptionsConfig {
   const values: string[] = Array.isArray (params) ? params : process.argv.slice (2)
   const args: ProgramOptionList = values.reduce (reduceArgs, [])
   const meta_init = { prior_option: null, non_null_key: null, config: {} }
